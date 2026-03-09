3339480160d618865d029b48af2acb4436b22274
FahadAli2001 <m.mohsin00933@gmail.com>
2026-03-09 06:10:49 -0700

    fahad

---
+++ src/loop.java
@@ -0,0 +1,24 @@
+
+public class loop {
+
+    public static void main(String[] args) {
+
+        // while loop 
+        // int i = 1;
+
+        // while (i <= 5) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+        //  do while
+
+        int b = 0;
+        do { 
+            b++;
+           System.out.println("Value of b is " + b);
+        } while (b<5);
+
+    }
+}
