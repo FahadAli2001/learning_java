7385111c6fc40e8283280502363ee8db1b192f07
FahadAli2001 <m.mohsin00933@gmail.com>
2026-03-10 06:50:44 -0700

    fahad

--- arrays_learning.java
+++ arrays_learning.java
@@ -1,10 +1,18 @@
 public class arrays_learning {
      public static void main(String[] args) throws Exception {
-        int [] marks = new int[5];
+        // int [] marks = new int[5];
 
-        for (int i = 0; i < marks.length; i++) {
-            marks[i] = i+1;
-            System.out.println(marks[i]);
+        // for (int i = 0; i < marks.length; i++) {
+        //     marks[i] = i+1;
+        //     System.out.println(marks[i]);
+        // }
+
+        int [] marks = {0,99,55,45,33,24,85,23};
+        // float [] marks1 = {10.5f,45.3f};
+        // String [] students = {"fahad","hamza","ali"};
+
+        for(int element:marks){
+            System.out.println(element);
         }
     }
 }
